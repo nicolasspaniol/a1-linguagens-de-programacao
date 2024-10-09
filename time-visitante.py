c197454afee560805da7c0d67b8eaa1761f4a929
João Vitor <joaovtaf@gmail.com>
2024-10-09 18:53:37 -0300

    criei o map dos países

--- time-visitante.py
+++ time-visitante.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-county = {}
+countries = {}
 
 club_games = pd.read_csv("dataset/club_games.csv")
 clubs = pd.read_csv("dataset/clubs.csv")
@@ -10,13 +10,11 @@ competitions = pd.read_csv("dataset/competitions.csv")
 
 games_away = club_games.loc[club_games.hosting == "Away"]
 
-for row in clubs.iterrows():
-    print(row)
-    country = competitions.loc[row[1]].country_name
-    # print(county)
-    # countries[row.club_id] = 
+for row in clubs.itertuples():
+    country = competitions.loc[competitions.competition_id == row.domestic_competition_id, "country_name"].values[0]
+    countries[row.club_id] = country
 
-# print(games_away.head())
+print(countries)
 
 # vitorias_fora
 # derrotas_fora
