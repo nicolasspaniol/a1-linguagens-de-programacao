210e021df3f3e58f7fbf9ce54885443492806051
nmspaniol <nicolasmspaniol@gmail.com>
2024-10-09 10:21:35 -0300

    Feito parte da hipótese

--- src/cartoes-posicao.py
+++ src/cartoes-posicao.py
@@ -1,19 +1,59 @@
 import pandas as pd
 import seaborn as sns
+import seaborn.objects as so
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 import numpy as np
+import textwrap
+
+
+def calc_rel_count(r):
+    return r["count"] / r["total_count"]
+
+
+def calc_total_count(r):
+    return ((merged["position"] == r["position"]) * merged["count"]).sum()
+
 
 sns.set_theme(style="ticks", palette="pastel")
 
-game_events = pd.read_csv('data/game_events.csv')
-players = pd.read_csv('data/players.csv')
+game_events = pd.read_csv("data/game_events.csv")
+game_lineups = pd.read_csv("data/game_lineups.csv")
 
+# Filtra apenas os eventos de cartões
 game_events = game_events.loc[game_events["type"] == "Cards"]
 
-game_events["card_type"] = game_events["description"].map(lambda e: "yellow" if "yellow" in e.lower() else "red")
-card_positions = pd.merge(game_events, players, on="player_id", how="left")
+game_events["card_type"] = game_events["description"] \
+    .map(lambda e: "yellow" if "yellow" in e.lower() else "red")
 
-print(card_positions.groupby(["card_type", "position"].size().reset_index(name="count")))
+merged = pd.merge(
+    game_events,
+    game_lineups,
+    how="inner",
+    on=["game_id", "player_id"]
+).groupby(["card_type", "position"]).size().reset_index(name="count")
 
-#sns.barplot(x=card_positions["position"], y=card_positions["card_type"])
-#plt.show()
+merged["total_count"] = merged.apply(calc_total_count, axis=1)
+merged["rel_count"] = merged.apply(calc_rel_count, axis=1)
+
+# Filtra posições com quantidade insuficiente de dados
+merged = merged.loc[merged["total_count"] > 150]
+print(f"n = {merged["count"].sum()}")
+
+# Gráfico de barras - quantidade de cartões recebidos por posição em campo
+ax = sns.barplot(
+    merged.loc[merged["card_type"] == "yellow"],
+    x="position",
+    y="count",
+)
+# Quebra o texto no eixo X em múltiplas linhas
+labels = [textwrap.fill(label.get_text(), 12) for label in ax.get_xticklabels()]
+ax.set_xticklabels(labels);
+plt.show()
+
+# Gráfico de barras empilhadas - proporção de cartões vermelhos/amarelhos por
+# posição em campo
+ax = so.Plot(merged, x="position", y="rel_count", color="card_type") \
+    .add(so.Bar(), so.Stack()) \
+    .scale(color={"yellow":"orange","red":"red"})
+ax.show()
