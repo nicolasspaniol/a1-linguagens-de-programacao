b27cb96ebb67c77d00d3366b37136ae0e52c133f
João Vitor <joaovtaf@gmail.com>
2024-10-09 22:55:42 -0300

    deu certo caralho

--- time-visitante.py
+++ time-visitante.py
@@ -1,24 +1,35 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
+import seaborn.objects as so
 
-countries = {}
 
 club_games = pd.read_csv("dataset/club_games.csv")
-clubs = pd.read_csv("dataset/clubs.csv")
-competitions = pd.read_csv("dataset/competitions.csv")
+games = pd.read_csv("dataset/games.csv")
 
-games_away = club_games.loc[club_games.hosting == "Away"]
+games_abroad = games.loc[games.competition_type == "international_cup"]
+games_home_country = games.loc[games.competition_type != "international_cup"]
 
-for row in clubs.itertuples():
-    country = competitions.loc[competitions.competition_id == row.domestic_competition_id, "country_name"].values[0]
-    countries[row.club_id] = country
+def calc_resultado(r):
+    if r['home_club_goals'] > r['away_club_goals']:
+        return 'derrota'
+    if r['home_club_goals'] == r['away_club_goals']:
+        return 'empate'
+    if r['home_club_goals'] < r['away_club_goals']:
+        return 'vitória'
+    
 
-print(countries)
+games["resultado"] = games.apply(calc_resultado, axis=1)
 
-# vitorias_fora
-# derrotas_fora
-# empates_fora
-# vitorias_exterior
-# derrotas_exterior
-# empates_exterior
+games["is_international"] = games["competition_type"] == "international_cup"
+
+fubá = games.groupby(["resultado", "is_international"]).size().reset_index(name="count")
+
+print(fubá)
+
+total_international = fubá.loc[fubá["is_international"]]["count"].sum()
+total_home = fubá.loc[~fubá["is_international"]]["count"].sum()
+fubá['count'] = np.where(fubá['is_international'] == True, fubá['count'] / total_international, fubá['count'] / total_home)
+
+ax = so.Plot(fubá, x="is_international", y="count", color="resultado") \
+    .add(so.Bar(), so.Stack())
+ax.show()
