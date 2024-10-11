6925c1b6ada9bd7d10ac41e6f938ef32a5d9359e
nmspaniol <nicolasmspaniol@gmail.com>
2024-10-11 17:16:38 -0300

    Limpeza e organização de código

--- src/time-visitante.py
+++ src/time-visitante.py
@@ -3,33 +3,35 @@ import numpy as np
 import seaborn.objects as so
 
 
-club_games = pd.read_csv("dataset/club_games.csv")
-games = pd.read_csv("dataset/games.csv")
+def calc_resultado(r):
+    # Para evitar mais consultas que o necessário
+    home_goals = r["home_club_goals"]
+    away_goals = r["away_club_goals"]
+
+    if home_goals > away_goals:
+        return "derrota"
+    if home_goals == away_goals:
+        return "empate"
+    if home_goals < away_goals:
+        return "vitória"
+
+
+club_games = pd.read_csv("data/club_games.csv")
+games = pd.read_csv("data/games.csv")
 
 games_abroad = games.loc[games.competition_type == "international_cup"]
 games_home_country = games.loc[games.competition_type != "international_cup"]
-
-def calc_resultado(r):
-    if r['home_club_goals'] > r['away_club_goals']:
-        return 'derrota'
-    if r['home_club_goals'] == r['away_club_goals']:
-        return 'empate'
-    if r['home_club_goals'] < r['away_club_goals']:
-        return 'vitória'
-    
 
 games["resultado"] = games.apply(calc_resultado, axis=1)
 
 games["is_international"] = games["competition_type"] == "international_cup"
 
-fubá = games.groupby(["resultado", "is_international"]).size().reset_index(name="count")
+games_frequency = games.groupby(["resultado", "is_international"]).size().reset_index(name="count")
 
-print(fubá)
+total_international = games_frequency.loc[games_frequency["is_international"]]["count"].sum()
+total_home = games_frequency.loc[~games_frequency["is_international"]]["count"].sum()
+games_frequency["count"] = np.where(games_frequency["is_international"] == True, games_frequency["count"] / total_international, games_frequency["count"] / total_home)
 
-total_international = fubá.loc[fubá["is_international"]]["count"].sum()
-total_home = fubá.loc[~fubá["is_international"]]["count"].sum()
-fubá['count'] = np.where(fubá['is_international'] == True, fubá['count'] / total_international, fubá['count'] / total_home)
-
-ax = so.Plot(fubá, x="is_international", y="count", color="resultado") \
+ax = so.Plot(games_frequency, x="is_international", y="count", color="resultado") \
     .add(so.Bar(), so.Stack())
 ax.show()
