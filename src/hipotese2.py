a4535c15b6b2464d3ed1323e9b7b19fee7ddfe6b
Gabrielly Chacara <gabriellyepc@gmail.com>
2024-10-11 14:21:16 -0300

    Adicionei a documentação dos módulos de H2 e H5

--- src/hipotese2.py
+++ src/hipotese2.py
@@ -1,14 +1,37 @@
 """
-Quais foram as compras de jogadores com melhores e 
-piores custo-benefício registradas?
+Módulo de Análise de Custo-Benefício de Compras de Jogadores.
+Este módulo processa e analisa dados de transferências de jogadores vindos de datasets 
+retirados do site de futebol "https://www.transfermarkt.com.br/" , performances e valores 
+de mercado para calcular o custo-benefício de contratações em termos de desempenho em 
+campo e variação no valor de mercado.
+
+Funções criadas:
+1º - Abertura dos arquivos CSV
+2º - Cálculo do custo-benefício dos jogadores
+3º - Correção de datas e valores de mercado ajustados pela inflação
+4º - Geração de gráficos para visualização dos resultados obtidos
 """
 
 from datetime import date, datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-import numpy as np
 import inflation
+
+"""
+Hipótese 2: Quais foram as compras de jogadores com melhores e piores custo-benefício registradas?
+"""
+
+#Função para abrir arquivos csv
+def open_csv(arquivo : str) -> pd.core.frame.DataFrame:
+    """
+    Abre um arquivo CSV na pasta '../data/' e carrega os dados em um DataFrame.
+
+        :param arquivo: Nome do arquivo CSV (sem a extensão '.csv').
+        :return: DataFrame contendo os dados do arquivo.
+    """
+    with open('../data/'+arquivo+'.csv', 'r', encoding='latin1') as file:
+        return pd.read_csv(file)
 
 #Função para calcular custo-beneficio
 def calc_cost_benefit(group : pd.core.frame.DataFrame) -> float:
@@ -22,8 +45,7 @@ def calc_cost_benefit(group : pd.core.frame.DataFrame) -> float:
         :return: Valor numérico que indica o custo-benefício do jogador.
     """
     delta_price = (group["market_value_in_eur_shift"].iloc[0] - group["market_value_in_eur"].iloc[0])
-    modificador = 10 ** (len(str(np.abs(int(delta_price))))-1)
-
+    modificador = 10 ** (len(str(delta_price))-1)
     #Fórmula para calcular desempenho do jogador, segundo sites esportivos (contem modificacao)
     reduce = sum((-1)*group["yellow_cards"] + (-3)*group["red_cards"] + (8)*group["goals"] + (5)*group["assists"]) * modificador / group.shape[0]
     return round((reduce + delta_price) / group["market_value_in_eur"].iloc[0], 4)
@@ -38,7 +60,7 @@ def correct_data_shift(row : pd.core.series.Series) -> datetime.date:
         :return: Data corrigida da transferência ou a data atual.
     """
     if not row["same_player"]:
-        return datetime.strptime("30/09/2024", "%d/%m/%Y").date()
+        return datetime.now().date()
     return row["date_shift"].date()
 
 #Função correção da market_value_in_eur_shift
@@ -55,10 +77,12 @@ def correct_market_value_in_eur_shift(row : pd.core.series.Series) -> float:
         return row["current_market_value"]
     return row["market_value_in_eur_shift"]
 
+
+
 #Abrindo as tabelas que serão utilizadas
-appearances = pd.read_csv('../data/appearances.csv')
-transfers = pd.read_csv('../data/transfers.csv')
-players = pd.read_csv('../data/players.csv')
+appearances = open_csv("appearances")
+players = open_csv("players")
+transfers = open_csv("transfers")
 
 #Limpando dados NaN das colunas que serão utilizadas
 appearances.dropna(axis=0, subset=["yellow_cards", "red_cards", "goals", "assists"], inplace=True) 
@@ -68,7 +92,6 @@ players.dropna(axis=0, subset=["name", "market_value_in_eur"], inplace=True)
 #Criando coluna same_player
 transfers.sort_values(['player_id', 'transfer_date'], ascending=True, inplace=True)
 transfers["transfer_date"] = pd.to_datetime(transfers["transfer_date"], yearfirst=True)
-transfers = transfers[transfers["transfer_date"] <= pd.to_datetime("30/09/2024", format="%d/%m/%Y")]
 transfers["same_player"] = -transfers["player_id"].diff(-1) == 0
 
 #Criando coluna data_shift
@@ -76,7 +99,7 @@ transfers['date_shift'] = transfers['transfer_date'].shift(-1)
 transfers["date_shift"] = transfers.apply(correct_data_shift, axis=1)
 
 #Criando coluna market_value_in_eur_shift
-transfers['market_value_in_eur'] = transfers.apply(lambda row: inflation.inflation_adj(row['market_value_in_eur'], row['transfer_date']), axis=1)
+transfers['market_value_in_eur'] = inflation.inflation_adj(transfers['market_value_in_eur'], transfers["transfer_date"])
 transfers['market_value_in_eur_shift'] = transfers['market_value_in_eur'].shift(-1)
 players.rename(columns={'market_value_in_eur': 'current_market_value'}, inplace=True)
 transfers = pd.merge(players[["player_id", "current_market_value"]], transfers, on='player_id')
@@ -94,11 +117,10 @@ merged.rename(columns={'player_name_x': 'player_name'}, inplace=True)
 #Agrupar por transferência
 cost_benefit = merged.groupby(['player_id', 'player_name', 'transfer_date', 'from_club_id', 'from_club_name', 'to_club_id', 'to_club_name']).apply(calc_cost_benefit).reset_index(name="cost_benefit")
 cost_benefit.sort_values(by='cost_benefit', ascending=False, inplace=True)
-
-print(cost_benefit)
+cost_benefit
 
 #Plotando o gráfico
 print(cost_benefit[["cost_benefit"]].describe())
 sns.boxplot(data=cost_benefit,  y="cost_benefit", color="red")
-plt.ylim(-5, 8)
 plt.show()
+
--- src/hipotese5.py
+++ src/hipotese5.py
@@ -1,5 +1,15 @@
 """
-Jogadores com preço fora do comum tem o desempenho proporcional?
+Módulo de Análise de Desempenho e Preço de Jogadores.
+Este módulo analisa a relação entre o valor de mercado fora do comum de jogadores e seu 
+desempenho em campo, determinando se os jogadores com preços inflacionados têm um desempenho 
+proporcional aos demais.
+
+Funções criadas:
+1º - Abertura dos arquivos CSV
+2º - Cálculo da média ponderada do valor de cada jogador
+3º - Cálculo do desempenho dos jogadores
+4º - Identificação dos outliers (jogadores com preço acima do limite superior)
+5º - Geração dos gráficos de correlação entre preço e desempenho
 """
 
 from datetime import date, datetime
@@ -9,10 +19,25 @@ import pandas as pd
 import numpy as np
 import inflation
 
+"""
+Hipótese 5: Jogadores com preço fora do comum tem o desempenho proporcional?
+"""
+
+#Função para abrir arquivos csv
+def open_csv(arquivo : str) -> pd.core.frame.DataFrame:
+    """ 
+    Abre um arquivo CSV na pasta '../data/' e carrega os dados em um DataFrame.
+
+        :param arquivo: Nome do arquivo CSV (sem a extensão '.csv').
+        :return: DataFrame contendo os dados do arquivo.
+    """
+    with open('../data/'+arquivo+'.csv', 'r', encoding='latin1') as file:
+        return pd.read_csv(file)
+
 #Função correção da data_diff
 def correct_data_diff(row : pd.core.series.Series) -> int:
     """ 
-    Corrige a diferença de datas entre registros de um jogador, ajustando para um limite de 365 dias se necessário.
+    Corrige a diferença de datas entre registros de um jogador, ajustando para um limite de 1 ano se necessário.
     Se o jogador mudou de clube (ou não é o mesmo jogador), a diferença entre as datas será calculada até o máximo de 365 dias.
     Caso contrário, mantém a diferença original de datas.
 
@@ -20,10 +45,10 @@ def correct_data_diff(row : pd.core.series.Series) -> int:
         :return: Diferença de dias entre as datas, ajustada ou original.
     """
     if not row["same_player"]:
-        return min((datetime.strptime("30/09/2024", "%d/%m/%Y").date() - row["date"].date()).days, 365)
+        return min((datetime.now().date() - row["date"].date()).days, 365)
     return row["date_diff"].days
 
-#Função que calcula media ponderada do valor do jogador
+#Função media ponderada do valor do jogador
 def calc_mean_price(group : pd.core.frame.DataFrame) -> float:
     """ 
     Calcula a média ponderada do valor de mercado de um jogador com base na duração de tempo em que ele manteve esse valor.
@@ -34,7 +59,6 @@ def calc_mean_price(group : pd.core.frame.DataFrame) -> float:
     """
     return round(sum(group["market_value_in_eur"] * group["date_diff"]) / sum(group["date_diff"]), 2)
 
-#Função que calcula performance do jogador
 def calc_performance(group : pd.core.frame.DataFrame) -> float:
     """ 
     Calcula o desempenho de um jogador com base em suas estatísticas de jogo, como gols, assistências e cartões.
@@ -46,7 +70,6 @@ def calc_performance(group : pd.core.frame.DataFrame) -> float:
     """
     return round(sum((-1)*group["yellow_cards"] + (-3)*group["red_cards"] + (8)*group["goals"] + (5)*group["assists"]) * 100 / group.shape[0], 4)
 
-#Função que calcula o limite superior
 def calc_upper_limit(col : pd.core.series.Series) -> float:
     """ 
     Calcula o limite superior para identificar outliers com base no intervalo interquartil.
@@ -61,18 +84,20 @@ def calc_upper_limit(col : pd.core.series.Series) -> float:
     return q3 + 1.5 * (q3 - q1)
 
 #Abrindo as tabelas que serão utilizadas
-appearances = pd.read_csv('../data/appearances.csv')
-player_valuations = pd.read_csv('../data/player_valuations.csv')
+appearances = open_csv("appearances")
+player_valuations = open_csv("player_valuations")
+players = open_csv("playes")
 
 #Limpando dados NaN das colunas que serão utilizadas
 appearances.dropna(axis=0, subset=["yellow_cards", "red_cards", "goals", "assists"], inplace=True) 
 player_valuations.dropna(axis=0, subset=["market_value_in_eur", "date"], inplace=True)
+players.drop(axis=0, subset=["name"], inplace=True)
 
 #Criando mean_price
 #Ordenar os dados, converter data, corrigir valores, criar colunas same_player e date_diff, calcular a média ponderada
 player_valuations.sort_values(['player_id', 'date'], ascending=True, inplace=True)
 player_valuations["date"] = pd.to_datetime(player_valuations["date"], yearfirst=True)
-player_valuations['market_value_in_eur'] = player_valuations.apply(lambda row: inflation.inflation_adj(row['market_value_in_eur'], row['date']), axis=1)
+player_valuations["market_value_in_eur"] = inflation.inflation_adj(player_valuations["market_value_in_eur"], player_valuations["date"])
 player_valuations["same_player"] = -player_valuations["player_id"].diff(-1) == 0
 player_valuations["date_diff"] = -player_valuations["date"].diff(-1)
 player_valuations["date_diff"] = player_valuations.apply(correct_data_diff, axis=1)
@@ -81,10 +106,10 @@ mean_price = player_valuations.groupby("player_id").apply(calc_mean_price).reset
 #Criando performance
 #Ordenar os dados, calcular performance do jogador
 appearances.sort_values('player_id', ascending=True, inplace=True)
-performance = appearances.groupby(["player_id", "player_name"]).apply(calc_performance).reset_index(name="performance")
+performance = appearances.groupby("player_id").apply(calc_performance).reset_index(name="performance")
 
 #Unindo os dados
-merged = pd.merge(performance, mean_price, how="left", on="player_id").dropna(axis=0).sort_values("mean_price", ascending=True)
+merged = pd.merge(players["player_id", "name"], performance, mean_price, how="left", on="player_id").dropna(axis=0).sort_values("mean_price", ascending=True)
 
 #Calculando limite superior
 merged["log_mean_price"] = np.log(merged["mean_price"])
@@ -96,3 +121,5 @@ print(performance_upper.describe())
 print(np.corrcoef(performance_upper["performance"], performance_upper["mean_price"])[0,1])
 sns.scatterplot(data=performance_upper, x="log_mean_price", y="performance", color="red")
 plt.show()
+
+
