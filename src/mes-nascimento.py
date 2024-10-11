7b9798f123d49ae923cfc6090e9effd03bf849b1
elisa-os <elisasoares2201@gmail.com>
2024-10-10 21:03:23 -0300

    Atualização

--- src/mes-nascimento.py
+++ src/mes-nascimento.py
@@ -7,12 +7,7 @@ from datetime import date, datetime
 sns.set_theme(style="ticks", palette="pastel")
 
 players = pd.read_csv('data/players.csv')
-
-
-def parse_date(date_str: str) -> date:
-    if len(date_str) > 10:
-        return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S').date()
-    return datetime.strptime(date_str, '%Y-%m-%d').date()
+players["date_of_birth"] = pd.to_datetime(players["date_of_birth"])
 
 
 def extract_month_of_birth(row):
@@ -24,7 +19,7 @@ def extract_month_of_birth(row):
     """
     birth_date = row["date_of_birth"]
     if type(birth_date) == str:
-        return parse_date(birth_date).month
+        return birth_date.month
     return None
 
 
