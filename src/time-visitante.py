4fb44488bc066b241108573046f941b51a41860b
nmspaniol <nicolasmspaniol@gmail.com>
2024-10-11 20:27:49 -0300

    Renomeando e calculando V de Cramer

--- src/time-visitante.py
+++ src/time-visitante.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import seaborn.objects as so
+from summary_statistics import cramer_v
 
 
 def calc_resultado(r):
@@ -22,16 +23,22 @@ games = pd.read_csv("data/games.csv")
 games_abroad = games.loc[games.competition_type == "international_cup"]
 games_home_country = games.loc[games.competition_type != "international_cup"]
 
-games["resultado"] = games.apply(calc_resultado, axis=1)
+games["result"] = games.apply(calc_resultado, axis=1)
 
 games["is_international"] = games["competition_type"] == "international_cup"
 
-games_frequency = games.groupby(["resultado", "is_international"]).size().reset_index(name="count")
+games_frequency = games.groupby(["result", "is_international"]).size().reset_index(name="count")
 
 total_international = games_frequency.loc[games_frequency["is_international"]]["count"].sum()
 total_home = games_frequency.loc[~games_frequency["is_international"]]["count"].sum()
-games_frequency["count"] = np.where(games_frequency["is_international"] == True, games_frequency["count"] / total_international, games_frequency["count"] / total_home)
 
-ax = so.Plot(games_frequency, x="is_international", y="count", color="resultado") \
-    .add(so.Bar(), so.Stack())
+pivot = games_frequency.pivot_table(index="result", columns="is_international", values="count")
+
+games_frequency["count"] = np.where(games_frequency["is_international"] == True, games_frequency["count"] /
+                                    total_international, games_frequency["count"] / total_home)
+
+print(f"V de Cramer das variáveis 'is_international' e 'result': {round(cramer_v(pivot), 2)}")
+ax = so.Plot(games_frequency, x="is_international", y="count", color="result") \
+    .add(so.Bar(), so.Stack()) \
+    .label(x="É internacional", y="Frequência relativa", color="Resultado da partida")
 ax.show()
