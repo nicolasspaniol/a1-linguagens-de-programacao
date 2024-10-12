a39832776593274e2f22ed21d1890d1055e0715c
nmspaniol <nicolasmspaniol@gmail.com>
2024-10-11 23:34:09 -0300

    Otimizando cálculo da inflação

--- src/inflation.py
+++ src/inflation.py
@@ -5,6 +5,20 @@ from datetime import datetime
 import math
 
 
+# Fonte: https://data.ecb.europa.eu/data/datasets/ICP/ICP.M.U2.N.000000.4.ANR
+df = pd.read_csv("data/hcpi/hcpi.csv")
+df["date"] = pd.to_datetime(df["date"], yearfirst=True)
+df["period"] = df["date"].map(lambda d: d.year * 12 + d.month)
+
+MIN_DATE_ID = df["period"].min()
+
+cum_inflation = []
+index = 100
+for i, r in df.iterrows():
+    index *= 1 + r["inflation"] / 1200
+    cum_inflation.append(index)
+
+
 def inflation_adj(value: float, period: datetime):
     """ Ajusta o valor monetário passado para a inflação atual (setembro de
         2024).
@@ -27,21 +41,6 @@ def inflation_adj(value: float, period: datetime):
     if period > datetime(2024, 9, 30):
         raise ValueError("O parâmetro 'period' deve representar uma data anterior a outubro de 2024")
 
-    period_str = period.strftime("%Y-%m")
-    now = df.loc[df["period"] == "2024-09"].iloc[0]["cum_inflation"]
-    then = df.loc[df["period"] == period_str].iloc[0]["cum_inflation"]
-    return round(value * now / then, 2)
-
-
-# Fonte: https://data.ecb.europa.eu/data/datasets/ICP/ICP.M.U2.N.000000.4.ANR
-df = pd.read_csv("data/hcpi/hcpi.csv")
-df["date"] = pd.to_datetime(df["date"], yearfirst=True)
-df["period"] = df["date"].dt.to_period("M").astype(str)
-
-cum_inflation = []
-index = 100
-for i, r in df.iterrows():
-    index *= 1 + r["inflation"] / 1200
-    cum_inflation.append(index)
-
-df["cum_inflation"] = pd.Series(cum_inflation)
+    now = cum_inflation[-1]
+    then = cum_inflation[period.year * 12 + period.month - MIN_DATE_ID]
+    return value * now / then
